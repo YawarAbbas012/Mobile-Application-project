450c2c23ad0c95729f7482a428d5183bd8791464
YawarAbbas012 <myawarpatoana@gmail.com>
2025-10-21 00:02:41 +0500

    wrap

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Wrap(
+        children: [
+          Container(width: 200, height: 200, color: Colors.amber),
+
+          Container(width: 200, height: 200, color: Colors.red),
+
+          Container(width: 200, height: 200, color: Colors.black),
+
+          Container(width: 200, height: 200, color: Colors.green),
+
+          Container(width: 200, height: 200, color: Colors.grey),
+
+          Container(width: 200, height: 200, color: Colors.pink),
+
+          Container(width: 200, height: 200, color: Colors.orange),
+
+          Container(width: 200, height: 200, color: Colors.deepPurple),
+
+          Container(width: 200, height: 200, color: Colors.deepOrange),
+        ],
+      ),
+    );
+  }
+}
